b9a1b9e6f5fd5e423958457a1e79d39da916ac24
ausaf-wpt <ausaf.wpt@gmail.com>
2022-05-06 23:46:43 +0500

    apis updated

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -4,20 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Message;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
+use App\Traits\ApiResponser;
+use App\Http\Resources\UserCollection;
 
 class ChatController extends Controller
 {
+
+    use ApiResponser;
     public function show(Request $request)
     {
         $userid = Auth::user()->id;
+        $from = Message::where('from_user_id', $userid)->pluck('to_user_id')->toArray();
+        $to = Message::where('to_user_id', $userid)->pluck('from_user_id')->toArray();
+
         $messages = Message::where(function ($query) use ($userid) {
             $query->where('from');
         })->orWhere(function ($query) use ($userid) {
             $query->where('to', $userid);
         })->get();
+        // if (($key = array_search($userid, $from)) !== false) {
+        //     unset($from[$key]);
+            
+        // }
+        $message_list = array_values(array_unique(array_merge($from,$to)));
 
-        return response()->json($messages);
+
+        return $this->success(['list'=>new UserCollection(User::whereIn('id',$message_list)->get())],'messages',200);
+
+        
     }
     public function store(Request $request)
     {
